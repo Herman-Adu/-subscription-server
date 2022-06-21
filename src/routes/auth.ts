147dcb7ab022287f4852ce344fcbf14d7f72154e
Herman Adu <herman.adu@hotmail.com>
2022-06-21 22:37:00 +0100

    added bcrypt, hashed password before saving to database, added create new user to auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
 import User from "../models/user";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -38,6 +39,13 @@ router.post(
         data: null,
       });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+    });
 
     res.json(user)
 });
